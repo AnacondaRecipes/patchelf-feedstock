04e3a72e70075a6f30aaa6691e2d84be5158aa88
Connor Martin <connormartin7@gmail.com>
2020-10-05 14:12:24 -0500

    change build.sj

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+./bootstrap.sh
 ./configure --prefix=${PREFIX}  \
             --host=${HOST}      \
             --build=${BUILD}
 make -j${CPU_COUNT} ${VERBOSE_AT}
+make check
 make tests
 make install
