0ab7962cbf7faa53ec07107d6622cf0a2429b4ba
Avalon <avalon@anaconda.com>
2022-10-26 11:05:14 -0700

    remove `-V` clang flag from configure step

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,13 @@
 cd ${SRC_DIR}
 
 ./bootstrap.sh
-./configure --prefix=${PREFIX}  \
+
+# -V does not exist for clang
+if [[ $(uname) == Darwin ]]; then
+  sed -i'' -e 's/for ac_option in --version -v -V -qversion; do/for ac_option in --version -v -qversion; do/g' 'configure'
+fi
+
+./configure -v --prefix=${PREFIX}  \
             --host=${HOST}      \
             --build=${BUILD}
 make -j${CPU_COUNT} ${VERBOSE_AT}
