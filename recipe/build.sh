7d6c8484a0b3c0fe56a89f72c663568393ad3aa3
Avalon <avalon@anaconda.com>
2022-10-27 13:17:07 -0700

    remove `-v` from configure step in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,14 @@ cd ${SRC_DIR}
 #  sed -i'' -e 's/for ac_option in --version -v -V -qversion; do/for ac_option in --version -v -qversion; do/g' 'configure'
 #fi
 
-./configure -v --prefix=${PREFIX}  \
+./configure --prefix=${PREFIX}  \
             --host=${HOST}      \
             --build=${BUILD}
 make -j${CPU_COUNT} ${VERBOSE_AT}
+
 if [[ $(uname) != Darwin ]]; then
   make check
   make tests
 fi
+
 make install
