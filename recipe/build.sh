fe45a902c428bc577ce2bb003c1b3279c3131c48
avalon (they/them) <77029455+uwuvalon@users.noreply.github.com>
2022-11-23 15:10:25 -0800

    update to 0.15.0 (#11)
    * bump version
    
    * update checksum
    
    * use clang and clangxx 14 in cbc.yaml
    
    * update source url
    
    * update patches
    
    * add binutils to linux build reqs
    
    * fix `about` pkg metadata

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-./bootstrap.sh
+cd ${SRC_DIR}
+
+# to fix configure version mismatch
+autoreconf
+
 ./configure --prefix=${PREFIX}  \
             --host=${HOST}      \
             --build=${BUILD}
 make -j${CPU_COUNT} ${VERBOSE_AT}
+
 if [[ $(uname) != Darwin ]]; then
   make check
   make tests
 fi
+
 make install
